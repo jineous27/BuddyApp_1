f3dfdc5187b0b5651fd86baa1b0f9d9c50edab5b
jinheeyi <jinhee.yi27@gmail.com>
2020-04-12 22:22:09 +1000

    Install morgan library - How to use

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const buddyIdRoutes = require('./routes/Buddy_ID');
 const buddyVehicleIdRoutes = require('./routes/Buddys_Vehicle');
 const buddyTaskId = require('./routes/Buddy_Task_ID');
 const deliveryOrderStatus = require('./routes/Delivery_Order_Status');
 
+app.use(morgan('dev')); //dev 버전으로 log를 찍겠다. 일반적인 dev version
 
 app.use('/api/buddyid', buddyIdRoutes);
 app.use('/api/vehicleid', buddyVehicleIdRoutes);
