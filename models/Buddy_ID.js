86559b26a49106f5b044dfda1862681199231c39
jinheeyi <jinhee.yi27@gmail.com>
2020-04-16 23:03:24 +1000

    몽고 Database 설계

---
+++ models/Buddy_ID.js
@@ -0,0 +1,59 @@
+// Model 이라는 폴더안에 각 JS 파일을 만들어서, 역할은 데이터베이스 구조설계를 위함이다. 몽고디비에 담기니깐 Mongoos 먼저 불러오기//
+const mongoose = require('mongoose');
+const buddyIdSchema = mongoose.Schema(
+    {
+
+        Delivery_Buddy_ID: {
+            type: Number,
+            required: true
+        },
+        Buddy_First_Name: {
+            type: String,
+            required: true
+        },
+        Buddy_Last_Name: {
+            type: String,
+            required: true
+        },
+        Buddy_Account_Email: {
+            type: String,
+            required: true
+        },
+        Buddy_Account: {
+            type: 
+
+        },
+        Agree_TermsConditions: {
+            type: 
+        },
+        Buddy_DOB: {
+            type: Date,
+            required: true,
+        },
+        Buddy_Work_Type: {
+
+        },
+        Buddy_ABN: {
+
+        },
+        Buddy_GST_Registration: {
+            type: Boolean,
+            default: false
+        },
+        Buddy_Contract: {
+
+        },
+        Buddy_Emergence: {
+
+        }
+
+    },
+    {
+        timestamps: true
+    }
+);
+
+
+
+
+module.exports = mongoose.model ("buddy_id", buddyIdSchema);
